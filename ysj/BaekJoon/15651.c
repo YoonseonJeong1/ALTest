7374b6a9f6ca380346ba2813864e6b25ecbae3dc
ysj-arad <seon1474@gmail.com>
2021-09-29 16:21:47 +0900

    baekjoon problem 15651 solve similar with problem 15649 easy problem

---
+++ ysj/BaekJoon/15651.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<string.h>
+
+char num[10];
+char ans[10];
+int a,b;
+void do_recursion(int n, int m)
+{
+	int i,j;
+	if(m == b)
+	{
+		for(j=0;j < b;j++)
+			printf("%c ",ans[j]);
+		printf("\n");
+		return;
+	}
+	for(i=1; i<=n; i++)
+	{
+		num[i]=1;
+		ans[m]=48+i;
+		do_recursion(n,m+1);
+		num[i]=0;
+		ans[m]=0;
+	}
+}
+
+int main(void)
+{
+	scanf("%d %d",&a,&b);
+	memset(num,0x0,sizeof(num));
+	do_recursion(a,0);
+
+	return 0;
+}
+
